10d240a9477d6b5a07915cc9f0add3f3ab58d4fe
Abhinav parbhakar <abhinav.parbhakar302@gmail.com>
2025-09-08 16:24:09 -0600

    Removed unrelated files and added logic for testing the validity of prompts

--- database_chat_integration.py
+++ database_chat_integration.py
@@ -3,17 +3,22 @@ from dotenv import load_dotenv
 from langchain_community.utilities import SQLDatabase
 from langgraph.prebuilt import create_react_agent
 from langgraph.graph.state import CompiledStateGraph
-from langchain_community.agent_toolkits import SQLDatabaseToolkit
+from langchain_community.agent_toolkits import SQLDatabaseToolkit, create_sql_agent
 import os
 
-def validate_prompt_topic(llm:ChatDeepSeek,prompt:str)->bool:
+
+def validate_information_needed_for_prompt(llm:ChatDeepSeek,database_connection_string:str,prompt:str)->bool:
     """
-    Given the prompt, use an LLM agent to check if the prompt aligns with a request for a SQL query for
-    a postgress database. 
+    Given the prompt, use an LLM agent to check if the prompt aligns with a request for a SQL query from 
+    the database schema. 
     
     ### Parameters
     1. llm:``langchain_deepseek.ChatDeepSeek``
         - Used to send the request
+    2. database_connection: ``str``
+        - Connection string needed to connect to the database.
+    3. prompt: ``str``
+        - The prompt to be tested
      
     ### Effects
     Internally makes a call to the LLM and depletes tokens
@@ -21,42 +26,47 @@ def validate_prompt_topic(llm:ChatDeepSeek,prompt:str)->bool:
     ### Returns
     ``True | False`` depending on closeness to a SQL query. 
     """
+    db = SQLDatabase.from_uri(database_connection_string)
     
-    system_content = """
-    You are an expert in writing SQL queries for a postgresql database. Your only job is to determine whether
-    the user prompt is asking for information that can returned by writing a SQL query to a database containing
-    information about traffic volume studies. Respond with 'true' if it is related and 'false' if it is not.
-    """
+    schema_info = db.get_table_info(db.get_usable_table_names())
+    
+    system_message = """You are an agent designed to interact with a SQL database.
+        The database information is this:
+        
+        {db_info}
+
+        Given an input question, check to see if the prompt can be converted into a {dialect} query based on
+        the schema and information in the database.
+
+
+        After examining the schema, analyze if the input question can be answered with the available data.
+        Simply respond with 'True' if enough information is present to answer the question, or 'False' if not enough information is available.
+        respond with anything else.
+        """.format(
+            db_info=schema_info,
+            dialect=db.dialect
+        )
+    
     messages = [
         (
             "system",
-            system_content
+            system_message
         ),
         (
             "human",
             prompt
         )
     ]
+    
+    
     response = llm.invoke(messages)
-    
-    if response.content.lower() == "false":
-        return False
-    elif response.content.lower() == "true":
-        return True
-    else:
-        raise Exception("Response from Validation LLM was not true or false")
 
-def query_from_database(llm:ChatDeepSeek,database_connection_string:str)->CompiledStateGraph:
-    db = SQLDatabase.from_uri(database_connection_string)
-    tools = SQLDatabaseToolkit(llm=llm,db=db)
-    
-    agent = create_react_agent(
-        model=llm,
-        tools=tools,
-        
-    )
-    
-    print(db.dialect)
+    if response.content.lower() == 'true':
+        return True
+    elif response.content.lower() == 'false':
+        return False
+    else:
+        raise Exception('Validation LLM did not return True or False')
 
 
 if __name__ == "__main__":
@@ -76,4 +86,7 @@ if __name__ == "__main__":
         api_key=api_key
     )
     
-    query_from_database(llm=llm,database_connection_string=database_connection_string)
+    print("Prompt:")
+    prompt = input()
+    
+    validate_information_needed_for_prompt(llm=llm,database_connection_string=database_connection_string,prompt=prompt)
