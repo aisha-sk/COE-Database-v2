0fb0a520340de68bcd87e706acb13357550c2a59
Abhinav parbhakar <abhinav.parbhakar302@gmail.com>
2025-09-30 18:39:46 -0600

    Edited  error handleding to server.py for /excel_file

--- server.py
+++ server.py
@@ -72,7 +72,7 @@ def configure_api_router(router:APIRouter,agent:SQLAgent)->APIRouter:
             media_type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
             return Response(content=excel_buffer.getvalue(),headers=headers,media_type=media_type)
         except Exception as e:
-            return JSONResponse(status_code=500,content={"Error":e})
+            return JSONResponse(status_code=200,content={"Error":e})
     
     return router
 
